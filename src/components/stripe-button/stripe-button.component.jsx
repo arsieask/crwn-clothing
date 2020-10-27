2f21a0a80ae71e57d2882f4e257dc38d16a57ae5
Arsen Askandaryan <arsieask@gmail.com>
2020-10-27 13:47:42 -0400

    updating stripe integration and updating our flavicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HeluJLDKEyY8fCFSXqEu0Rw6clzwW3AGwtfILELg86k3bqagVcvegz1Ij5vsUo6q2F9EhPEVqjApaJOteQ5rELm005seO9OBm';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
